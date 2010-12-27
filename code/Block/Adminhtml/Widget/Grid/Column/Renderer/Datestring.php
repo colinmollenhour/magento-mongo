0b192170f6447d21f0f5ed5ced6c7eb02857e1a7
colinmollenhour <colin@mollenhour.com>
2010-12-27 17:42:50 -0500

    Add helper for mongo-specific column renderers.

---
+++ code/Block/Adminhtml/Widget/Grid/Column/Renderer/Datestring.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Adminhtml grid item renderer for mongo type "datestring"
+ */
+
+class Cm_Mongo_Block_Adminhtml_Widget_Grid_Column_Renderer_Datestring extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
+{
+  protected $_defaultWidth = 160;
+  /**
+   * Date format string
+   */
+  protected static $_format = null;
+
+  /**
+   * Retrieve date format
+   *
+   * @return string
+   */
+  protected function _getFormat()
+  {
+    $format = $this->getColumn()->getFormat();
+    if (!$format) {
+      if (is_null(self::$_format)) {
+        try {
+          self::$_format = Mage::app()->getLocale()->getDateFormat(
+            Mage_Core_Model_Locale::FORMAT_TYPE_MEDIUM
+          );
+        }
+        catch (Exception $e) {
+
+        }
+      }
+      $format = self::$_format;
+    }
+    return $format;
+  }
+
+  /**
+   * Renders grid column
+   *
+   * @param   Varien_Object $row
+   * @return  string
+   */
+  public function render(Varien_Object $row)
+  {
+    if ($data = $row->getData($this->getColumn()->getIndex())) {
+      $format = $this->_getFormat();
+      $data = Mage::app()->getLocale()->date($data, Varien_Date::DATE_INTERNAL_FORMAT)->toString($format);
+      return $data;
+    }
+    return $this->getColumn()->getDefault();
+  }
+}
--- code/Helper/Data.php
+++ code/Helper/Data.php
@@ -3,4 +3,21 @@
 class Cm_Mongo_Helper_Data extends Mage_Core_Helper_Abstract
 {
   
+  /**
+   * Get all mongo column renderers or just one.
+   * 
+   * @param string $key
+   * @return mixed
+   */
+  public function getGridColumnRenderers($key = NULL)
+  {
+    $renderers = array(
+      'datestring' => 'mongo/adminhtml_widget_grid_column_renderer_datestring',
+    );
+    if($key !== NULL) {
+      return isset($renderers[$key]) ? $renderers[$key] : NULL;
+    }
+    return $renderers;
+  }
+  
 }
