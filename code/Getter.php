e4865888992f67a974602df01a956b2c214a01d9
colinmollenhour <colin@mollenhour.com>
2011-01-11 17:50:47 -0500

    Added type conversion abilities to Cm_Mongo_Getter.

--- code/Getter.php
+++ code/Getter.php
@@ -6,22 +6,36 @@ class Cm_Mongo_Getter
   /** @var array */
   protected $_parts;
 
+  /** @var string */
+  protected $_type;
+
   /**
    * Useful for the getter property of a grid.
    *
    * 'getter' => Cm_Mongo_Getter::factory('getFoo.getBar'),
+   * 'getter' => Cm_Mongo_Getter::factory('getFoo.getUpdatedAt', 'timestamp'),
    *
-   * @param string $path
+   * @param string $path    Dot-delimited path to desired data
+   * @param string $type    Optional data type to convert value to
    */
-  public static function factory($path)
+  public static function factory($path, $type = NULL)
   {
-    $getter = new self($path);
-    return array($getter, 'fetch');
+    $getter = new self($path, $type);
+    return array($getter, $type === NULL ? 'fetch' : 'fetchAsType');
   }
 
-  public function  __construct($path)
+  /**
+   * Recommended to instead use static factory method.
+   *
+   * @see Cm_Mongo_Getter::factory()
+   *
+   * @param string $path    Dot-delimited path to desired data
+   * @param string $type    Optional data type to convert value to
+   */
+  public function  __construct($path, $type = NULL)
   {
     $this->_parts = explode('.',$path);
+    $this->_type = $type;
   }
 
   /**
@@ -37,6 +51,18 @@ class Cm_Mongo_Getter
       $value = $value->{$this->_parts[$i]}();
     }
     return $value;
+  }
+
+  /**
+   * Fetch the value and convert the type using the tophp converter.
+   *
+   * @param Varien_Object $object
+   * @return mixed
+   */
+  public function fetchAsType(Varien_Object $object)
+  {
+    $value = $this->fetch($object);
+    return Mage::getSingleton('mongo/type_tophp')->{$this->_type}(NULL, $value);
   }
 
 }
