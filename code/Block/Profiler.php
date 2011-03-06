9aacff4ecd16fbd84a211ea1c1e531d3ef77c425
colinmollenhour <colin@mollenhour.com>
2011-03-06 15:28:44 -0500

    Minor stuff

--- code/Block/Profiler.php
+++ code/Block/Profiler.php
@@ -17,7 +17,7 @@ class Cm_Mongo_Block_Profiler extends Mage_Core_Block_Abstract
 
     ob_start();
     echo '<div style="background:white; text-align: left; padding: 1em;">';
-    echo '<style>#mongo_profiler td { padding: 1px 3px; } #mongo_profiler th { padding: 3px; font-size: 120%; text-align: center; } </style>';
+    echo '<style type="text/css">#mongo_profiler td { padding: 1px 3px; } #mongo_profiler th { padding: 3px; font-size: 120%; text-align: center; } </style>';
     echo "<a href=\"#\" onclick=\"$('mongo_profiler').toggle(); return false;\">[mongo profiler]</a>";
     echo '<table id="mongo_profiler" border="1" cellpadding="2" style="width:auto; border-collapse: collapse; margin: 1em;">';
     echo '<tr><th>Query</th><th>Time</th><th>Count</th></tr>';
