0f6fd0b3a01729dbc9f58c36851367e26c4bb5b3
Colin Mollenhour <colin@mollenhour.com>
2012-02-17 16:32:25 -0500

    Map mongodb-php-odm files to autoload-friendly locations.

--- code/Model/Resource/Type/Mongo.php
+++ code/Model/Resource/Type/Mongo.php
@@ -11,10 +11,6 @@ class Cm_Mongo_Model_Resource_Type_Mongo extends Mage_Core_Model_Resource_Type_A
    */
   public function getConnection(Mage_Core_Model_Config_Element $config)
   {
-    require_once 'mongodb-php-odm'.DS.'classes'.DS.'json.php';
-    require_once 'mongodb-php-odm'.DS.'classes'.DS.'mongo'.DS.'database.php';
-    require_once 'mongodb-php-odm'.DS.'classes'.DS.'mongo'.DS.'collection.php';
-
     $conn = Mongo_Database::instance((string)$config->config, $config->asCanonicalArray());
 
     // Set profiler
